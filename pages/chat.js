f713c9d671d1ad1154d6aaa9af19bb219a96a17e
Felipe Pereira Gaigher <67787756+FelipeGaigher@users.noreply.github.com>
2022-02-01 16:26:36 -0300

    att - aula 4

--- pages/chat.js
+++ pages/chat.js
@@ -1,20 +1,55 @@
 import { Box, Text, TextField, Image, Button } from "@skynexui/components";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import appConfig from "../config.json";
+import { createClient } from '@supabase/supabase-js'
+
+
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY3MjY5MCwiZXhwIjoxOTU5MjQ4NjkwfQ.qsnte9cqPtBrSapTQd1rQLFuXJ51wRpueJ1Ll8W_t_o";
+const SUPABASE_URL = "https://tptpsqogzlfhtxvqfffa.supabase.co";
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 
 export default function ChatPage() {
   const [mensagem, setMensagem] = React.useState("");
   const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
 
+  useEffect(() => {
+      supabaseClient
+      .from('mensagens')
+      .select('*')
+      .order('id', {ascending: false})
+      .then(({ data }) => {
+        console.log('Dados da consulta: ', data)
+        setListaDeMensagens(data)
+      })
+  }, []);
+  
+
   function handleNovaMensagem(novaMensagem) {
     const mensagem = {
-      id: listaDeMensagens.length + 1,
-      de: "FelipeGaigher",
+      // id: listaDeMensagens.length + 1,
+      de: 'FelipeGaigher',
       texto: novaMensagem,
     };
-    setListaDeMensagens([mensagem ,...listaDeMensagens ]);
+
+    supabaseClient
+      .from('mensagens')
+      .insert([ 
+        mensagem
+      ])
+      .then(({data}) => {
+        console.log("criando mensagem: ", data)
+        setListaDeMensagens([
+          data[0], 
+          ...listaDeMensagens
+          ]);
+      })
+
     setMensagem("");
   }
+
+
 
   /*
     // Usuario:
@@ -32,17 +67,16 @@ export default function ChatPage() {
       styleSheet={{
         display: "flex",
         alignItems: "center",
-        justifyContent: "right",
+        justifyContent: "center",
         backgroundColor: appConfig.theme.colors.primary[500],
         backgroundImage:
-            "url(https://virtualbackgrounds.site/wp-content/uploads/2020/12/amazon-rainforest.jpg)",        
+          "url(https://virtualbackgrounds.site/wp-content/uploads/2020/12/amazon-rainforest.jpg)",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         backgroundBlendMode: "light",
         color: appConfig.theme.colors.neutrals["000"],
       }}
     >
-      
       <Box
         styleSheet={{
           display: "flex",
@@ -57,7 +91,6 @@ export default function ChatPage() {
           padding: "32px",
         }}
       >
-      
         <Header />
         <Box
           styleSheet={{
@@ -100,7 +133,6 @@ export default function ChatPage() {
                   handleNovaMensagem(mensagem);
                 }
               }}
-
               placeholder="Insira sua mensagem aqui..."
               type="textarea"
               styleSheet={{
@@ -163,8 +195,6 @@ function MessageList(props) {
         return (
           <Text
             key={mensagem.id}
-            onClick={() => handleRemoveClick(item.id)}
-
             tag="li"
             styleSheet={{
               borderRadius: "5px",
@@ -188,7 +218,8 @@ function MessageList(props) {
                   display: "inline-block",
                   marginRight: "8px",
                 }}
-                src={`https://github.com/felipegaigher.png`}
+                src={`https://github.com/${mensagem.de}.png`}
+                
               />
               <Text tag="strong">{mensagem.de}</Text>
               <Text
@@ -201,6 +232,12 @@ function MessageList(props) {
               >
                 {new Date().toLocaleDateString()}
               </Text>
+              {/* <Button
+                variant="tertiary"
+                colorVariant="neutral"
+                label="X"
+                href="/"
+              /> */}
             </Box>
             {mensagem.texto}
           </Text>
